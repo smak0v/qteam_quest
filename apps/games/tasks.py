926a6f6fe01392bcf470a4f780294d11ab573ece
alexey_ignatov <alexey.ignatov.msu@gmail.com>
2020-01-14 17:32:18 +0300

    debug logging and dev task fix

--- apps/games/tasks.py
+++ apps/games/tasks.py
@@ -1,6 +1,10 @@
 from celery.schedules import crontab
 from celery.task import periodic_task
 from django.utils import timezone
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 from apps.games.models import Game
 
@@ -9,8 +13,10 @@ from apps.games.models import Game
                name='close_registration_one_hour_before_the_game',
                ignore_result=True)
 def close_registration_one_hour_before_the_game():
-    games = Game.objects.filter(timespan__year=timezone.now().year,
-                                timespan__month=timezone.now().month,
-                                timespan__day=timezone.now().day)
+    #games = Game.objects.filter(timespan__year=timezone.now().year,
+    #                            timespan__month=timezone.now().month,
+    #                            timespan__day=timezone.now().day)
+    games = Game.objects.all()
+    logger.debug("close_registration_one_hour_before_the_game")
     for game in games:
         game.check_registration_availability()
