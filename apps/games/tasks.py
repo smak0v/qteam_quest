d4761d4e93cdbc1b42e7ffb17b203675f7b26dc5
Sergey Makov <gromfighter@gmail.com>
2020-01-15 19:58:43 +0200

    Refactor

--- apps/games/tasks.py
+++ apps/games/tasks.py
@@ -1,19 +1,19 @@
+import logging
+
 from celery.schedules import crontab
 from celery.task import periodic_task
-from django.utils import timezone
-import logging
+
+from apps.games.models import Game
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
-from apps.games.models import Game
 
 
 @periodic_task(run_every=(crontab(minute='*/1')),
                name='close_registration_one_hour_before_the_game',
                ignore_result=True)
 def close_registration_one_hour_before_the_game():
-    #games = Game.objects.filter(timespan__year=timezone.now().year,
+    # games = Game.objects.filter(timespan__year=timezone.now().year,
     #                            timespan__month=timezone.now().month,
     #                            timespan__day=timezone.now().day)
     games = Game.objects.all()
